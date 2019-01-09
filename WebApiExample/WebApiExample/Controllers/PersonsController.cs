a2dc032bd4028643caf959966dd78fa5dc9e988a
SannaNaTu <korhonsm@gmail.com>
2019-01-09 15:06:18 +0200

    Added database and PersonsController. Some changes to Startup, appsettings.json files

---
+++ WebApiExample/WebApiExample/Controllers/PersonsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiExample.Models;
+
+namespace WebApiExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+   
+        // GET api/values
+        [HttpGet]
+        public ActionResult GetPersons()
+        {
+            var users = new List<Person>
+            {
+
+                new Person("James",45),
+                new Person("Lissu",34),
+              };
+            return new JsonResult(users);
+        }
+
+        // GET api/values/5 HAKU
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value AB";
+        }
+
+    
+
+    }
+}
---
+++ WebApiExample/WebApiExample/Models/Tehtävä1Context.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApiExample.Models
+{
+    public partial class Tehtävä1Context : DbContext
+    {
+        public Tehtävä1Context()
+        {
+        }
+
+        public Tehtävä1Context(DbContextOptions<Tehtävä1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<Phone> Phone { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Data Source=DESKTOP-9CKPS05\\SQLEXPRESS;Initial Catalog=Tehtävä1;Integrated Security=True");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.Name).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Phone>(entity =>
+            {
+                entity.Property(e => e.Number).IsUnicode(false);
+
+                entity.HasOne(d => d.Person)
+                    .WithMany(p => p.Phone)
+                    .HasForeignKey(d => d.PersonId)
+                    .HasConstraintName("FK_Phone_Person");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
